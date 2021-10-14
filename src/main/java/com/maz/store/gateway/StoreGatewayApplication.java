c011f3c68e3f6b4824876ae39ac008977fa9de1c
Ayoub <ayoubzoubeidi@gmail.com>
2021-10-14 17:49:29 +0100

    Initial project commit

---
+++ src/main/java/com/maz/store/gateway/StoreGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.maz.store.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreGatewayApplication.class, args);
+    }
+
+}
