7a94094a8885f05bfd3825014184744b1a243cf5
Ayoub <ayoubzoubeidi@gmail.com>
2021-10-18 16:07:31 +0100

    misc

--- src/main/java/com/maz/store/gateway/config/LocalDiscoveryRouteConfig.java
+++ src/main/java/com/maz/store/gateway/config/LocalDiscoveryRouteConfig.java
@@ -13,7 +13,6 @@ public class LocalDiscoveryRouteConfig {
         return
                 builder.routes()
 
-                        //todo add routes for product service, order service and inventory service
                         .route(r -> r.path("product_route", "/api/v1/products/**").uri("lb://product-service"))
                         .route(r -> r.path("inventory_route", "/api/v1/inventory/**").uri("lb://inventory-service"))
                         .route(r -> r.path("order_route", "/api/v1/orders/**").uri("lb://order-service"))
